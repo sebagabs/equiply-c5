e38663fed0fcba11bad24c72e3d603f1fd86f178
Sebastian G Maldonado-rosado <sgm7@gatech.edu>
2026-02-14 09:12:28 -0400

    Methods can now take more than 3 points as input

--- main.py
+++ main.py
@@ -23,10 +23,12 @@ def calculate_helper(coordinates: list[dict[str, float]]) -> dict:
 
 
 def distance(coordinates: list[dict[str, float]]) -> float:
-    p1 = (coordinates[0].get('x', 0), coordinates[0].get('y', 0), coordinates[0].get('z', 0))
-    p2 = (coordinates[1].get('x', 0), coordinates[1].get('y', 0), coordinates[1].get('z', 0))
-    p3 = (coordinates[2].get('x', 0), coordinates[2].get('y', 0), coordinates[2].get('z', 0))
-    return round(math.dist(p1, p2) + math.dist(p2, p3), 2)
+    result = 0.0
+    for i in range(len(coordinates) - 1):
+        p1 = (coordinates[i].get('x', 0), coordinates[i].get('y', 0), coordinates[i].get('z', 0))
+        p2 = (coordinates[i+1].get('x', 0), coordinates[i+1].get('y', 0), coordinates[i+1].get('z', 0))
+        result += math.dist(p1, p2)
+    return round(result, 2)
 
 
 def speed(coordinates: list[dict[str, float]]) -> float:
@@ -36,9 +38,9 @@ def speed(coordinates: list[dict[str, float]]) -> float:
 
 
 def velocity(coordinates: list[dict[str, float]]) -> dict[str, float]:
-    time_elapsed = coordinates[2].get('timestamp', 1) - coordinates[0].get('timestamp', 0)
+    time_elapsed = coordinates[len(coordinates)-1].get('timestamp', 1) - coordinates[0].get('timestamp', 0)
     return {
-        'x': round((coordinates[2].get('x', 0) - coordinates[0].get('x', 0)) / time_elapsed, 2) if time_elapsed > 0 else 0,
-        'y': round((coordinates[2].get('y', 0) - coordinates[0].get('y', 0)) / time_elapsed, 2) if time_elapsed > 0 else 0,
-        'z': round((coordinates[2].get('z', 0) - coordinates[0].get('z', 0)) / time_elapsed, 2) if time_elapsed > 0 else 0,
+        'x': round((coordinates[len(coordinates)-1].get('x', 0) - coordinates[0].get('x', 0)) / time_elapsed, 2) if time_elapsed > 0 else 0,
+        'y': round((coordinates[len(coordinates)-1].get('y', 0) - coordinates[0].get('y', 0)) / time_elapsed, 2) if time_elapsed > 0 else 0,
+        'z': round((coordinates[len(coordinates)-1].get('z', 0) - coordinates[0].get('z', 0)) / time_elapsed, 2) if time_elapsed > 0 else 0,
     }
